6901515bfc5252c660cda7ee8ad1e150018bdb2b
Kevin Today <ktoday@palantir.com>
2016-02-19 02:11:21 +0000

    Added streaks

--- analyze-full-clear-days.py
+++ analyze-full-clear-days.py
@@ -28,6 +28,43 @@ def _validate_args(args):
     """ Performs validation on the given args dict, returning a non-zero exit code if errors were found or None if all is well """
     return None
  
+def _calculate_runs(datapoints):
+    """
+    Helper function to calculate runs of:
+    0. streaks - consecutive days of full-clears
+    1. droughts - consecutive days 
+
+    Args:
+    datapoints -- A list of datapoints with at least one element
+    """
+    current_streak = None
+    streaks = []
+
+    prev_datapoint = None
+    for datapoint in datapoints:
+        date = datapoint[_DATE_KEY]
+        if prev_datapoint is not None:
+            prev_date = prev_datapoint[_DATE_KEY]
+            
+            if (date - prev_date).days == 1:
+                if current_streak is None:
+                    current_streak = [prev_datapoint, datapoint]
+                    streaks.append(current_streak)
+                else:
+                    current_streak[1] = datapoint
+            else:
+                current_streak = None
+
+        prev_datapoint = datapoint
+
+    # For convenience, we'll calculate lengths
+    streaks = [(streak[0], streak[1], (streak[1][_DATE_KEY] - streak[0][_DATE_KEY]).days + 1) for streak in streaks]
+
+    # TODO Implmement droughts
+    # We now have a dict of streaks as (start, end) pairs, so we can calculate droughts from there
+    return (streaks, [])
+
+
 def main(argv):
     args = _parse_args(map(str, argv))
     err = _validate_args(args)
@@ -45,9 +82,16 @@ def main(argv):
         parsed_date, parse_type = cal.parse(datapoint[_DATE_STR_KEY])
         datapoint[_DATE_KEY] = datetime.date(parsed_date[0], parsed_date[1], parsed_date[2]) if parse_type == 1 else None
 
+    """
     days_of_week = [datapoint[_DATE_KEY].weekday() for datapoint in datapoints]
     plt.hist(days_of_week, 7)
     plt.show()
+    """
+
+    streaks, droughts = _calculate_runs(datapoints)
+    sorted_streaks = sorted(streaks, key=lambda streak: streak[2])
+    longest_streak = sorted_streaks[-1]
+    print "Longest streak: {} days between {} and {}".format(longest_streak[2], longest_streak[0][_DATE_KEY], longest_streak[1][_DATE_KEY])
  
     return 0
  
